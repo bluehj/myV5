247f2e92186e3a51b571a32512c5db2c428f7c7e
bluehj <he.fifteen@gmail.com>
2014-02-04 13:49:03 +0800

    查找数据, 创建分页
    查找数据, 创建分页

--- db/Control/NewsControl.class.php
+++ db/Control/NewsControl.class.php
@@ -2,8 +2,30 @@
 
     class NewsControl extends Control{
 
+        //显示文章列表
+        function show(){
+        	$db = M("news");
+        	//分页
+        	//文章总条数
+        	$count = $db->count();
+        	$page = new Page($count, 5);
+        	$this->assign('page', $page->show());
+        	$data = $db->where($page->limit())->all();
+        	$this->assign('news', $data);
+            $this->display();
+        }
+
         //向文章表news表添加内容
    	    function add(){
-            $this->display();
+   	    	if(IS_POST){
+   	    		$db = M("news");
+   	    		if($db->add()){
+   	    	        $this->success("发表成功","show");
+   	    		} else{
+   	    		    $this->error("发表失败","show");
+   	    		}
+   	    	} else{
+   	    	    $this->display();	
+   	    	}
    	    }
     }
--- db/Temp/Compile/add_eb126.php
+++ db/Temp/Compile/add_eb126.php
@@ -1,1 +1,40 @@
-<?php if(!defined("HDPHP_PATH"))exit;C("SHOW_NOTICE",FALSE);?>ok...
+<?php if(!defined("HDPHP_PATH"))exit;C("SHOW_NOTICE",FALSE);?><html>
+<head>
+    <title>添加文章</title>
+</head>
+<body>
+	<form action='http://localhost/myV5/db.php/News/add' method="POST">
+    <table>
+        <tr>
+            <td>标题</td>
+            <td><input type="text" name="title" /></td>
+        </tr>
+        <tr>
+            <td>正文</td>
+            <td><script charset="utf-8" src="http://localhost/hdphp/hdphp/Extend/Org/Keditor/kindeditor-all-min.js"></script>
+            <script charset="utf-8" src="http://localhost/hdphp/hdphp/Extend/Org/Keditor/lang/zh_CN.js"></script>
+        <textarea id="hd_content" name="content"></textarea>
+    <script>
+        var options_content = {
+        filterMode : false
+                ,id : "editor_id"
+        ,width : "100%"
+        ,height:"300px"
+                ,formatUploadUrl:false
+        ,allowFileManager:false
+        ,allowImageUpload:true
+        ,uploadJson : "http://localhost/myV5/db.php/News&m=keditor_upload&editor_type=2&image=1&uploadsize=2000000&maximagewidth=false&maximageheight=false&hd_sid=tfs7a0jembno7s2p2f79ij7de2"//处理上传脚本
+        };var hd_content;
+        KindEditor.ready(function(K) {
+                    hd_content = KindEditor.create("#hd_content",options_content);
+        });
+        </script>
+        </td>
+        </tr>
+        <tr>
+            <td colspan=2><input type="submit" value="提交文章" /></td>
+        </tr>
+    </table>
+    </form>
+</body>
+</html>
